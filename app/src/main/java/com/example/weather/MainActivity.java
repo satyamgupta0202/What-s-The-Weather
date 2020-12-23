17d9e18aacc78754caa991d252e7bd0fe64fcc7f
satyam <2019335@iiitdmj.ac.in>
2020-12-23 22:43:54 +0530

    Initial commit

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,13 +2,71 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    public class DownloadTask extends AsyncTask<String,Void,String>{
 
+        @Override
+        protected String doInBackground(String... urls) {
+            URL url;
+            HttpURLConnection connection = null;
+            String result = "";
+            try {
+                url =  new URL(urls[0]);
+                connection = (HttpURLConnection)url.openConnection();
+                InputStream in = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data=reader.read();
+                while(data!=-1){
+                    char current = (char)data;
+                    result = result+current;
+                    data= reader.read();
+                }
+                return result;
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+                JSONArray arr = new JSONArray(weatherInfo);
+                for(int i=0;i<4;i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                    Log.i("main",jsonPart.getString("main"));
+                    Log.i("description",jsonPart.getString("description"));
+
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DownloadTask task = new DownloadTask();
+        task.execute(" ");
     }
 }
