2f1dd85539f63ee916027ed1792cda83a5a6545d
satyam <2019335@iiitdmj.ac.in>
2020-12-24 21:02:45 +0530

    FinallyDone

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -18,17 +21,20 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     EditText city;
-
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         city = findViewById(R.id.editText);
+        result = findViewById(R.id.result);
     }
 
     public void getWeather(View view) {
         DownloadTask task = new DownloadTask();
         task.execute("http://api.openweathermap.org/data/2.5/weather?q="+city.getText().toString()+"&APPID=be1eab311204153c5b99974ec7df464c");
+        InputMethodManager mgr = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(city.getWindowToken(),0);
     }
     public class DownloadTask extends AsyncTask<String,Void,String>{
 
@@ -51,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
                 return result;
             }
             catch (Exception e){
+                Toast.makeText(getApplicationContext(),"could not find weather",Toast.LENGTH_LONG).show();
                 e.printStackTrace();
+
                 return null;
             }
         }
@@ -63,13 +71,25 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject = new JSONObject(s);
                 String weatherInfo = jsonObject.getString("weather");
                 JSONArray arr = new JSONArray(weatherInfo);
-                for(int i=0;i<4;i++){
+                String string = "";
+                for(int i=0;i<arr.length();i++){
                     JSONObject jsonPart = arr.getJSONObject(i);
-                    Log.i("main",jsonPart.getString("main"));
-                    Log.i("description",jsonPart.getString("description"));
+                    String main =  jsonPart.getString("main");
+                    String description=jsonPart.getString("description");
+                    if(!main.equals("") && !description.equals(""))
+                    string += main + ": " + description + "\r\n";
+                }
+                if(!string.equals(""))
+                    result.setText(string);
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"could not find weather",Toast.LENGTH_SHORT).show();
+
                 }
             }
             catch (Exception e){
+
+                Toast.makeText(getApplicationContext(),"could not find weather",Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
         }
